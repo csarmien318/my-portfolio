20aad97828184d5fb8c8ab7eb05e22e053366f9b
Christopher Sarmiento <csarmien318@gmail.com>
2021-12-28 16:28:42 -0500

    Added groundwork for navbar and pages

---
+++ src/components/projects.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Projects extends React.Component {
+  render() {
+    return <h1>Projects go here with sidebar to navigate through works.</h1>;
+  }
+}
+
+export default Projects;
