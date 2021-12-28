99a5bbfdc0c753afdbd47046137f8bba84d5bbbb
Christopher Sarmiento <csarmien318@gmail.com>
2021-12-28 17:46:25 -0500

    Created service folder and added songs and artists

---
+++ src/services/artistService.js
@@ -0,0 +1,9 @@
+export const artists = [
+  { _id: "5b21ca3eeb7f6fbccd471818", name: "Pink Floyd" },
+  { _id: "5b21ca3eeb7f6fbccd471814", name: "The Smiths" },
+  { _id: "5b21ca3eeb7f6fbccd471820", name: "Radiohead" },
+];
+
+export function getArtists() {
+  return genres.filter((g) => g);
+}
