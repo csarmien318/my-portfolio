4474b9998cd2e587d334a7cb3f543a663cb9f914
Christopher Sarmiento <csarmien318@gmail.com>
2022-02-03 13:34:40 -0500

    Updating branch

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,16 +1,57 @@
 import React from "react";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 
 class Contact extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Contact Form</h1>
-        <ul>
-          <li>Name</li>
-          <li>Email</li>
-          <li>Message</li>
-          <li>Submit Button</li>
-        </ul>
+      <div className="container col-5">
+        <Card>
+          <Card.Header>Contact Me</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form className="mb-3">
+                <Row>
+                  <Col>
+                    <Form.Control placeholder="Full name" />
+                  </Col>
+                  <Col>
+                    <Form.Control placeholder="Company (optional)" />
+                  </Col>
+                </Row>
+              </Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                  isInvalid
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid email (ex: johndoe@email.com).
+                </Form.Control.Feedback>
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Write your message here..."
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
