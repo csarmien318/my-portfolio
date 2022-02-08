42f5e8028ef019ecfd7c946c73c00ad9e5f94b78
csarmien318 <96742273+csarmien318@users.noreply.github.com>
2022-02-08 11:50:55 -0500

    Revert "Dev"

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -60,24 +60,19 @@ export const Weather = () => {
             {weather && (
               <div style={{ margin: "20px" }}>
                 <Card>
-                  <Card.Title as="h5">{`${weather.city}${
-                    weather.state && weather.city !== weather.state
-                      ? ", " + weather.state
-                      : ""
-                  }, ${weather.country}`}</Card.Title>
                   <Card.Body>
                     <Container>
                       <Row>
                         <Col xs={12} md={4}>
-                          <p>{`${weather.conditions}`}</p>
-                          <Card.Img src={weather.icon} alt="weather icon" />
+                          <div>{`${weather.city}, ${weather.state} ${weather.country}`}</div>
                         </Col>
                         <Col
                           xs={12}
                           md={8}
                           className="d-flex flex-column justify-content-between"
                         >
-                          <h1>{`${Math.round(weather.temperature)} \xB0F`}</h1>
+                          <span>{`${weather.temperature} F`}</span>
+                          <span>{weather.conditions}</span>
                         </Col>
                       </Row>
                     </Container>
