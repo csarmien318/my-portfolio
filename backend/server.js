1e24de6c55fc18247b04be5586defd6dcd722711
Christopher Sarmiento <csarmien318@gmail.com>
2022-04-26 13:50:13 -0400

    Deployment bug fix two

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "my-portfolio", "build", "index.html"));
+    res.sendFile(
+      path.join(__dirname, "../my-portfolio", "../build", "../build/index.html")
+    );
   });
 } else {
   app.get("/", (req, res) => {
