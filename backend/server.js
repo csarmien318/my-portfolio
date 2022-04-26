28754d5c7887e056fdb70283c451011f16bb01a5
Christopher Sarmiento <csarmien318@gmail.com>
2022-04-26 18:53:06 -0400

    Deployment bug fix twelve

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(morgan("tiny"));
 app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "my-portfolio", "build", "index.html"));
