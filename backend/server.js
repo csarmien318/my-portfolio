5f5ba8ba47d70c6a6d500b59444284e802fb53bf
Christopher Sarmiento <csarmien318@gmail.com>
2022-04-27 12:18:02 -0400

    Reconfigured files for deployment

--- backend/server.js
+++ backend/server.js
@@ -30,17 +30,17 @@ app.use(
 
 // HTTP request logger
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "build/")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "..", "build/")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api running");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Api running");
+//   });
+// }
 
 app.use(morgan("tiny"));
 app.use("/api", routes);
