08ca8afa37701307b812708051924d5345b48c11
Christopher Sarmiento <csarmien318@gmail.com>
2022-04-27 19:14:42 -0400

    Deployment bug fix twenty

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const path = require("path");
 const cors = require("cors");
 
@@ -28,11 +28,12 @@ app.use(
   })
 );
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("client/build")));
+  // app.use(express.static(path.join("client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
