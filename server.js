218286f0771c39b4ba55f6068e74cddd28b01a90
Christopher Sarmiento <csarmien318@gmail.com>
2022-04-27 22:28:52 -0400

    Deployment bug fix twenty three

--- server.js
+++ server.js
@@ -36,19 +36,19 @@ app.use(
 // app.use(morgan("tiny"));
 app.use("/api", routes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/client/build")));
-//   // app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  // app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//     // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Api is running");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running");
+  });
+}
 
 app.listen(
   process.env.PORT,
